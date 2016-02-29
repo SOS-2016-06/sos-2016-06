83a993084b4a9758310eced4e1a5a8f7b597e646
root <root@mac-debian>
2016-02-29 19:06:20 +0100

    Page about data  closes #7

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var app = express();
+var fs=require("fs");
+
 
 // CallBack Asíncrono
 app.get("/",(req,res) => {
-  res.send("Raíz");
+var r = '<a href="/about">About Us</a>'
+
+  res.send(r);
 });
 
 app.get("/about",(req,res) => {
@@ -42,8 +46,68 @@ app.get("/about/f1championship",(req,res) => {
 
 });
 
+
+
+
+app.get("/about/olympicgames",(req,res) => {
+  var games=[];
+  console.log(" Request OlimpicsGames");
+  var r = "<html><head><h1>Olimpics Games</h1></head>"+
+  "<body>"+
+    "<table border='2px'>"+
+    "<tr>"+
+      "<td><strong>city</strong></td>"+
+      "<td><strong>year</strong></td>"+
+      "<td><strong>sports</strong></td>"+
+      "<td><strong>athletes</strong></td>"+
+      "<td><strong>modality</strong></td>"+
+    "</tr>"
+  res.write(r);
+
+      fs.readFile('olimpicgames.json','utf8',(err,content)=>{
+        //ASinc
+        console.log("Read data");
+       games= JSON.parse(content);
+
+  
+  
+
+        games.forEach((rawGame)=>{
+          res.write(
+          "<tr>"+
+          "<td>"+rawGame.city+"</td>"+
+          "<td>"+rawGame.year+"</td>"+
+          "<td>"+rawGame.sportsnumber+"</td>"+
+          "<td>"+rawGame.athletes+"</td>"+
+          "<td>"+rawGame.modality+"</td>"+
+          "</tr>");
+  
+          });
+
+        res.end();
+      });
+    
+
+
+  
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 //////// PUERTO USADO EN HEROKU /////
-app.listen(process.env.PORT); /////
+app.listen(8080);
+//app.listen(process.env.PORT); /////
 /////////////////////////////////////
 
 //app.listen(5000); // Descomentar solo para pruebas en local
