561ab4e114e60eb3ff260e99b7747bdde65dddc7
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-04-17 18:34:02 +0200

    Quique table client closes  #44

---
+++ static/js/olympicstable.js
@@ -0,0 +1,67 @@
+$( document ).ready(function() {
+
+	console.log("jquery Ready")
+
+    var request = $.ajax({
+ 			url: '/api/v1/olympicsgames?apikey=user',
+ 			type:"GET",
+			contentType: "application/json",
+ 			
+ 			
+		});
+		
+
+request.done(function(dataJson) {
+       
+        	var table = buildTable(dataJson);
+        	$("#tableOlympics").html(table);
+        	console.log(status);
+       
+               
+		});
+
+
+request.always(function(jqXHR, status, statusText) {
+ 				// Tratamiento en cualquier caso
+ 				console.log(status)
+
+ 				if (status=="error"){
+ 					$("#tableOlympics").html("Not Resources yet");
+
+ 				}
+				
+			});
+
+		
+
+
+
+function buildTable(dataJson) {
+		var table = $('<table width="800" border = 2 >');
+		var start = '<tr align="right" >';
+				for (var header in dataJson[0]) {
+
+        	start += '<th>' + header + '</th>';
+    	}
+
+		start += '</tr>';
+		$(start).appendTo(table);
+		$.each(dataJson, (index, element) => {
+			  var row = '<tr align="right">';
+			  $.each(element, (key, val) => {
+				    row += '<td align="right"> ' + val + '</td>';
+			  });
+			  row += '</tr>';
+
+			  $(table).append(row);
+		});
+		$(table).append('</table>');
+
+		return table;
+};
+		
+
+
+
+
+});
