bfc10371177c139688ff15046778f9ac9603cf3d
leobernalb <leobernal91@gmail.com>
2016-04-18 14:01:12 +0200

    Build Table. Closes #43

---
+++ static/js/f1championships.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {
+
+  var request = $.ajax({
+    url: '/api/v1/f1championships?apikey=user',
+    type:"GET",
+    contentType: "application/json",
+  });
+
+
+  request.done(function(data) {
+    var table = $('<table width="800" border = 2 >');
+    // Header
+		var start = '<tr align="right" >';
+    for (var header in data[0]) {
+      start += '<th>' + header + '</th>';
+    }
+    start += '</tr>';
+		$(start).appendTo(table);
+
+    // Body
+    $.each(data, (index, element) => {
+      var row = '<tr align="right">';
+			$.each(element, (key, val) => {
+        row += '<td align="right"> ' + val + '</td>';
+			});
+			row += '</tr>';
+
+      $(table).append(row);
+		});
+
+    $(table).append('</table>');
+    $("#tableF1").html(table);
+    console.log(status);
+  });
+
+
+  request.always(function(jqXHR, status, statusText) {
+    console.log(status);
+   	   	if (status=="error"){
+      $("#tableF1").html("No data load");
+   	}
+  });
+
+});
