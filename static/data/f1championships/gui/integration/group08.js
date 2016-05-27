2da3a1c28c307c5bb755ec16d45c61c212c46207
leobernalb <leobernal91@gmail.com>
2016-05-28 01:12:15 +0200

    Integration 1

--- static/data/f1championships/gui/integration/group08.js
+++ static/data/f1championships/gui/integration/group08.js
@@ -1,34 +1,45 @@
-var request_06 = $.ajax({
-  url: '/api/v1/f1championships?apikey=multiPlan_C1_sos-2016-06-lbb_ag',
-  type:"GET",
-  contentType: "application/json",
-});
 
-var request_08 = $.ajax({
-  url: '/api/v1/music?apikey=multiPlan_C5_sos-2016-08-bhl_ag',
-  type:"GET",
-  contentType: "application/json",
-});
+$(document).ready(() => {
 
-request_06.done(function(response) {
+  var request_06 = $.ajax({
+    url: 'https://sos-2016-06.herokuapp.com/api/v1/f1championships?apikey=multiPlan_C1_sos-2016-06-lbb_ag',
+    type:"GET",
+    contentType: "application/json",
+  });
+
+  var request_08 = $.ajax({
+    url: 'https://sos-2016-08.herokuapp.com/api/v1/social_situation/?apikey=multiPlan_C4_sos-2016-08-cmg_ag',
+    type:"GET",
+    contentType: "application/json",
+  });
+
+  var dataW = [['Country','Year']]; // Cabezera
+
+  request_06.done(function(response) {
+    $.each(response, function (i, item) { // Recorremos la respuesta y obtenemos el "i" y cada elemento JSON "item"
+        dataW.push([item.country,item.year]);
+    });
+  });
+
+  request_08.done(function(response) {
+    $.each(response, function (i, item) {
+        dataW.push([item.country,item.year]);
+    });
+  });
+
+  // Widget Google Chart
   google.charts.load('current', {'packages': ['geochart']});
   google.charts.setOnLoadCallback(drawMarkersMap);
 
   function drawMarkersMap() {
-    var f1 = [['City','Wins']];
-
-    $.each(response, function (i, item) {
-        f1.push([item.country,item.wins]);
-    });
-
-    var data = google.visualization.arrayToDataTable(f1);
-
+    var data = google.visualization.arrayToDataTable(dataW);
     var options = {
       displayMode: 'markers',
-      colorAxis: {colors: ['green', 'blue']}
+      colorAxis: {colors: ['green','blue', 'red']}
     };
 
     var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
     chart.draw(data, options);
   };
+
 });
